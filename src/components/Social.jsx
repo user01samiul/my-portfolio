0f67b75b058d0c611e0bd5210d93b1e829523bf3
Md. Samiul Islam <samiul.primary@gmail.com>
2024-05-04 16:03:58 +0600

    changes

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -9,7 +9,7 @@ function Social() {
       <div className="grid2 ">
         <Social1 ext="social1" />
         <Social2
-          icon="x.png"
+          icon="github.png"
           title="X"
           cardColor="bg-[rgb(0,0,0,0.07)] hover:bg-[rgb(0,0,0,0.14)] active:bg-[rgb(0,0,0,0.14)]"
           btnColor="bg-black hover:bg-[#090d35] active:bg-[#090d35]"
@@ -18,22 +18,31 @@ function Social() {
         />
 
         <Social2
-          icon="facebook.png"
-          title="Facebook"
-          cardColor="bg-[rgb(24,119,242,0.07)] hover:bg-[rgb(24,119,242,0.14)] active:bg-[rgb(24,119,242,0.14)]"
-          btnColor="bg-[#1877F2] hover:bg-[#1a529c] active:bg-[#1a529c]"
-          id="Samiul Islam"
-          ext="social3"
-        />
-        <div className="social4 socialBoxx"></div>
-        <Social2
           icon="instagram.png"
           title="Instagram"
           cardColor="bg-[rgb(232,115,86,0.07)] hover:bg-[rgb(232,115,86,0.14)] active:bg-[rgb(232,115,86,0.14)]"
           btnColor="bg-gradient-to-r from-[rgba(75,96,202)] to-[rgba(255,118,68)] hover:from-[#3047be] active:from-[#3047be] hover:to-orange-700 active:to-orange-700"
           id="_sa.mii__"
+          ext="social3"
+        />
+        <div className="social4 socialBoxx"></div>
+        <Social2
+          icon="x.png"
+          title="X"
+          cardColor="bg-[rgb(0,0,0,0.07)] hover:bg-[rgb(0,0,0,0.14)] active:bg-[rgb(0,0,0,0.14)]"
+          btnColor="bg-black hover:bg-[#090d35] active:bg-[#090d35]"
+          id="unfazedsami"
           ext="social5"
         />
+        <Social2
+          icon="facebook.png"
+          title="Facebook"
+          cardColor="bg-[rgb(24,119,242,0.07)] hover:bg-[rgb(24,119,242,0.14)] active:bg-[rgb(24,119,242,0.14)]"
+          btnColor="bg-[#1877F2] hover:bg-[#1a529c] active:bg-[#1a529c]"
+          id="Samiul Islam"
+          ext="social6"
+        />
+
         <div className="social6 socialBoxx"></div>
       </div>
     </div>
